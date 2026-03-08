c02cca61cab9f369d76af3e4381e45e41ca9e118
nezkie001 <nezkie829@gmail.com>
2026-03-08 21:03:50 +0800

    Add teacher interface functions

---
+++ js/teacher.js
@@ -0,0 +1,21 @@
+// Functions for Teacher Interface
+
+function addTeacher(name, subject) {
+    // Code to add a new teacher
+    console.log(`Teacher ${name} added for subject ${subject}.`);
+}
+
+function removeTeacher(name) {
+    // Code to remove an existing teacher
+    console.log(`Teacher ${name} removed.`);
+}
+
+function listTeachers() {
+    // Code to list all teachers
+    console.log("Listing all teachers...");
+}
+
+function assignTeacherToClass(teacherName, className) {
+    // Code to assign a teacher to a class
+    console.log(`Teacher ${teacherName} assigned to class ${className}.`);
+}
